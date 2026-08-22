d1675fef0af63b5e810251dd0fc67264de7d1515
developergracz <64929957+developergracz@users.noreply.github.com>
2026-08-22 23:34:39 +0200

    security: harden legacy PHP sessions and error logging

--- website/wykonanie_procedur_startowych.php
+++ website/wykonanie_procedur_startowych.php
@@ -10,13 +10,19 @@ $load = sys_getloadavg();
 if ($load[0] > 80) {
     header('HTTP/1.1 503 Too busy, try again later');
     die('<meta charset="utf8" /><div style="width:70%; font-size:200%; margin:auto; margin-top:300px; background:f5f5f5; border-radius:10pt;">Przepraszamy,<br />nasz serwer jest zbyt obciążony. Spróbuj ponownie później.<br /><br /><span style="font-size:300%;">;(</span></div>');
-    $filename = fopen($directory['log'].'too_busy.log','w+');
-    fwrite($filename,'Server too busy at: '.date('Y-m-d h:m:s')."\r\n");
-    fclose($filename);
 }
 
-// We are setting HTTP-ONLY parameter to our cookies
-session_set_cookie_params(30*24*60*60, '/', $domain, false, true);
+// Harden session cookies. Respect HTTPS terminated by a trusted reverse proxy.
+$is_https = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ||
+            (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) === 'https');
+ini_set('session.use_strict_mode', '1');
+ini_set('session.use_only_cookies', '1');
+ini_set('session.use_trans_sid', '0');
+session_set_cookie_params(30*24*60*60, '/', $domain, $is_https, true);
+
+header('X-Content-Type-Options: nosniff');
+header('X-Frame-Options: SAMEORIGIN');
+header('Referrer-Policy: strict-origin-when-cross-origin');
 
 // Włączernie/Wyłączenie wyświetlania błędów
 ini_set('display_errors',$production_mode?"Off":"On");
@@ -28,7 +34,7 @@ else
 
 // Włączanie kompresji GZIP
 if(!ini_get('zlib.output_compression')){
-  if(substr_count($_SERVER['HTTP_ACCEPT_ENCODING'],'gzip')){
+  if(isset($_SERVER['HTTP_ACCEPT_ENCODING']) && substr_count($_SERVER['HTTP_ACCEPT_ENCODING'],'gzip')){
     ini_set('zlib.output_compression_level',1);
     ob_start('ob_gzhandler');
   }
@@ -41,9 +47,17 @@ function savePHPError($kod_bledu, $opis_bledu, $plik_wystapienia, $linia)
   $opis_bledu = addslashes(htmlspecialchars($opis_bledu));
   $plik_wystapienia = addslashes(htmlspecialchars($plik_wystapienia));
   $linia = intval($linia);
-  $parametry = addslashes(htmlspecialchars(print_r($_REQUEST,true)));
-  $url = addslashes(htmlspecialchars($_SERVER['REQUEST_URI']));
-  $IP = addslashes(htmlspecialchars($_SERVER['REMOTE_ADDR']));
+
+  // Never write credentials, session ids or CSRF/reset tokens into application logs.
+  $safe_request = $_REQUEST;
+  foreach (array('password','password2','old_password','new_password','token','PHPSESSID','authorization') as $sensitive_key)
+  {
+    if (isset($safe_request[$sensitive_key]))
+      $safe_request[$sensitive_key] = '[REDACTED]';
+  }
+  $parametry = addslashes(htmlspecialchars(print_r($safe_request,true)));
+  $url = addslashes(htmlspecialchars(isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : ''));
+  $IP = addslashes(htmlspecialchars(isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : ''));
   if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
     $proxy = addslashes(htmlspecialchars($_SERVER['HTTP_X_FORWARDED_FOR']));
   else
@@ -67,8 +81,6 @@ function UnhandledErrorsCatcher($errno, $errstr, $errfile, $errline)
   if(in_array($errno, array(E_ERROR, E_WARNING, E_PARSE, E_RECOVERABLE_ERROR)))
   {
     savePHPError($errno,$errstr,$errfile,$errline);
-    // UWAGA - poniższa instrukcja throw ma skłonność do wypluwania parametrów funkcji na wyjście (w tym haseł w mysql_connect() ) !!!
-    //throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
   }
   return false;
 }
@@ -78,7 +90,8 @@ function UnhandledExceptionsCatcher($exception)
   global $path;
   $plik = fopen($path['log_exceptions_php'],'a+');
   if (!$plik) return;
-  fwrite($plik,print_r($exception,true)."\r\n");
+  // Do not serialize request data or credentials in the exception log.
+  fwrite($plik,get_class($exception).': '.$exception->getMessage()."\r\n");
   fwrite($plik,'==========================================='."\r\n");
   fclose($plik);
   return false;
@@ -98,37 +111,17 @@ if (isset($_SERVER['X-Purpose'])&&$_SERVER['X-Purpose']=='preview')
   exit();
 }
 
-// Ustawianie lokalizacji // powoduje błędy przy liczbach zmiennoprzecinkowych (przecinek w lokalizacji polskiej jest przecinkiem a nie kropką)
-//$arrLocale = array( "pl_PL", "polish_pol" );
-//setlocale( LC_ALL, $arrLocale );
-
-if (isset($_POST['PHPSESSID']))
-{
-  session_id($_POST['PHPSESSID']);
-}
-
-
 session_start();
 
-//echo '(przesłany)'.$_REQUEST['token'].'=(nowy)'.$_SESSION['token'].',(stary)'.$_SESSION['token_last'];
 include_once($actual_path.'library_main.php'); // przetworzenie 40ms
 
-//Rejstracja wywołań
-//$p = fopen('logi/aaa='.date('Y-m-d h.m.s'),'w+');
-//fwrite($p, 'IP='.$_SERVER['REMOTE_ADDR']);
+DatabaseConnect(); // ok. 10ms
 
-DatabaseConnect(); // ok. 10ms (ale zauważ, że gdy rozłączone, to nie tracimy czasu na wyk. zapytań)
-
-// Wysyłanie różnego Content-Type w zależności od przeglądarki (dla IE XHTML należy dozować jako text/html ;)
-// Również w zalezności od tego czy jest to zapytanie Ajaxowe czy normalne
 header('Content-Type: text/html; charset=UTF-8;');
 srand();
 
-// It must be before TOKEN initialization (TOKEN is initialized in AuthorizeUser function)
-// If user enter ActiveAccount page, we are logging him out.
 if (strpos($_SERVER['REQUEST_URI'], $path['activate_account'])>0)
   Logout();
-
 
 $komunikat_logowania = '';
 try
@@ -138,26 +131,24 @@ try
     AuthorizeUser($_POST['login'], $_POST['password'], isset($_POST['remember_me'])?($_POST['remember_me']=='on'?true:false):false);
   }else
   {
-    AuthorizeUser(); // po zalogowaniu, bez większego wpływu czasowego
+    AuthorizeUser();
   }
 }catch(ExceptionRoot $e)
 {
   $komunikat_logowania = $e;
 }
 
-ProtectAgainstSessionHijacking(); // bez większego wpływu czasowego
+ProtectAgainstSessionHijacking();
 
-if (IsIPAddressBlocked($_SERVER['REMOTE_ADDR'])) // wahania ms - bez większego wpływu czasowego
+if (IsIPAddressBlocked($_SERVER['REMOTE_ADDR']))
 {
   echo('Nie masz dostępu do serwisu ponieważ Twój adres IP został zablokowany przez administratora.');
   exit();
 }
 
-// Jeśli kliknięto przycisk rejestracji przez Facebook
 if (isset($_REQUEST['facebookRegister']))
   facebookRedirectToFacebookLoginPage();
 
-// Przekierowanie na stronę główną, gdy nie udało się zarejestrować konta przez Facebooka
 if (isset($_REQUEST['error_reason'])&&($_REQUEST['error_reason']=='user_denied'))
   header('Location: '.$service_base_address."\r\n");
 
