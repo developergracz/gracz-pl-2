eb1cf1836e44c08333fb467db30a83aec9d25f17
developergracz <64929957+developergracz@users.noreply.github.com>
2026-08-22 23:34:15 +0200

    security: require POST for abuse mutations

--- website/ajaxAbuse.php
+++ website/ajaxAbuse.php
@@ -1,59 +1,79 @@
-<?php include("variables_local.php"); include_once($actual_path."wykonanie_procedur_startowych.php"); ?>
+<?php
+include("variables_local.php");
+include_once($actual_path."wykonanie_procedur_startowych.php");
 
-  <?php
-    //header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
+header('X-Content-Type-Options: nosniff');
 
-    switch ($_REQUEST['action'])
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  http_response_code(405);
+  header('Allow: POST');
+  echo json_encode(array(
+    'state' => 'error',
+    'message' => 'Method not allowed.'
+  ));
+  include_once($actual_path."wykonanie_procedur_koncowych.php");
+  exit;
+}
+
+$action = isset($_POST['action']) ? (string)$_POST['action'] : '';
+
+switch ($action)
+{
+  case 'report':
+  {
+    try{
+      ReportAbuse(
+        isset($_POST['address']) ? $_POST['address'] : '',
+        isset($_POST['description']) ? $_POST['description'] : '',
+        isset($_POST['token']) ? $_POST['token'] : ''
+      );
+      echo(json_encode(array(
+        'state' => 'reported',
+        'message' => 'Twoje zgłoszenie zostało przyjęte. Dziękujemy!'
+      )));
+    }catch (Exception $e)
     {
-
-      case 'report':
-      {
-        try{
-          ReportAbuse($_REQUEST['address'], $_REQUEST['description'], $_REQUEST['token']);
-          echo(json_encode(array(
-            'state' => 'reported',
-            'message' => 'Twoje zgłoszenie zostało przyjęte. Dziękujemy!'
-          )));
-        }catch (Exception $e)
-        {
-          echo(json_encode(array(
-            'state' => 'error',
-            'message' => $e->getMessage()
-          )));
-        }
-
-        break;
-      }
-
-
-      case 'delete':
-      {
-        try{
-          DeleteAbuseReport($_REQUEST['id_abuse'], $_REQUEST['token']);
-          echo(json_encode(array(
-            'state' => 'deleted'
-          )));
-        }catch (Exception $e)
-        {
-          echo(json_encode(array(
-            'state' => 'error',
-            'message' => $e->getMessage()
-          )));
-        }
-        break;
-      }
-
-
-      default:
-      {
-        echo(json_encode(array(
-          'state' => 'error',
-          'message' => 'Unsupported action.'
-        )));
-        break;
-      }
-
+      http_response_code(400);
+      echo(json_encode(array(
+        'state' => 'error',
+        'message' => $e->getMessage()
+      )));
     }
-  ?>
+    break;
+  }
 
-<?php include_once($actual_path."wykonanie_procedur_koncowych.php"); ?>
+  case 'delete':
+  {
+    try{
+      DeleteAbuseReport(
+        isset($_POST['id_abuse']) ? $_POST['id_abuse'] : '',
+        isset($_POST['token']) ? $_POST['token'] : ''
+      );
+      echo(json_encode(array(
+        'state' => 'deleted'
+      )));
+    }catch (Exception $e)
+    {
+      http_response_code(400);
+      echo(json_encode(array(
+        'state' => 'error',
+        'message' => $e->getMessage()
+      )));
+    }
+    break;
+  }
+
+  default:
+  {
+    http_response_code(400);
+    echo(json_encode(array(
+      'state' => 'error',
+      'message' => 'Unsupported action.'
+    )));
+    break;
+  }
+}
+
+include_once($actual_path."wykonanie_procedur_koncowych.php");
+?>
