79310303faf49957d744d38936acd320b7b58e96
developergracz <64929957+developergracz@users.noreply.github.com>
2026-08-22 23:54:31 +0200

    security: validate game ranking tab input

--- website/gry_ranking.php
+++ website/gry_ranking.php
@@ -2,78 +2,31 @@
 
 <?php
 
-  echo('
-  <div class="panel_lewy">
-  ');
-    WyswietlLewyPanel($_SESSION['account_type']);   
-  echo('
-  </div>
-  ');   
+echo('<div class="panel_lewy">');
+WyswietlLewyPanel(isset($_SESSION['account_type']) ? $_SESSION['account_type'] : 0);
+echo('</div>');
 
-   
-if ($_SESSION['account_type'] < USER)
+if (!isset($_SESSION['account_type']) || $_SESSION['account_type'] < USER)
 {
-
-  echo('   
-  <div class="tresc">
-    <div class="uwaga">Nie zalogowałeś się do tej pory. Jedynie zalogowani użytkownicy mają dostęp do tej strony.</div>
-  </div>
-  ');   
-
-}else
+  http_response_code(403);
+  echo('<div class="tresc"><div class="uwaga">Nie zalogowałeś się do tej pory. Jedynie zalogowani użytkownicy mają dostęp do tej strony.</div></div>');
+}
+else
 {
-  echo('
-  <div class="tresc">  
-  ');
-  
-  echo('<h1>Ranking gier</h1>
-  
-  <div id="ranking_gier">
-    <ul>
-      <li><a href="#gry_najnowsze">Najnowsze gry</a></li>
-      <li><a href="#gry_najpopularniejsze">Najpopularniejsze gry</a></li>
-    </ul>
+  echo('<div class="tresc"><h1>Ranking gier</h1><div id="ranking_gier"><ul><li><a href="#gry_najnowsze">Najnowsze gry</a></li><li><a href="#gry_najpopularniejsze">Najpopularniejsze gry</a></li></ul><div id="gry_najnowsze">');
+  GryWyswietlNajnowsze();
+  echo('</div><div id="gry_najpopularniejsze">');
+  GryWyswietlNajpopularniejsze();
+  echo('</div></div>');
 
-    <div id="gry_najnowsze">
-    ');
-    
-    GryWyswietlNajnowsze();
-    
-    echo('
-    </div>  
-    <div id="gry_najpopularniejsze">
-    ');
-    
-    GryWyswietlNajpopularniejsze();  
+  $karta = isset($_GET['karta']) ? intval($_GET['karta']) : 0;
+  if ($karta < 0 || $karta > 1) $karta = 0;
 
-    echo('
-    </div>
-    
-  </div>
-  ');
-  
-  $karta = intval($_GET['karta']);
-  if ($karta<0||$karta>1) $karta = 0;
-
-  echo('  
-  <script type="text/javascript">
-  //<![CDATA[
-    jQuery("#ranking_gier").tabs();
-    jQuery("#ranking_gier").tabs().tabs("select",'.$karta.');
-  //]]>
-  </script>
-  ');
-  
-  
-  
+  echo('<script type="text/javascript">jQuery("#ranking_gier").tabs(); jQuery("#ranking_gier").tabs().tabs("select",'.$karta.');</script>');
   echo('<br /><br />');
   WyswietlLinkiNawigacyjne(true, false, true, true);
-  
-  echo('
-  </div>
-  ');
+  echo('</div>');
 }
-   
 ?>
 
 <?php include_once($footer); ?>
