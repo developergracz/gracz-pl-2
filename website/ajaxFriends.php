f070f624a6eac489087ef05a882a53d07f6c124b
developergracz <64929957+developergracz@users.noreply.github.com>
2026-08-22 23:38:28 +0200

    security: require POST for friend mutations

--- website/ajaxFriends.php
+++ website/ajaxFriends.php
@@ -1,51 +1,43 @@
 <?php include("variables_local.php"); include_once($actual_path."wykonanie_procedur_startowych.php"); ?>
+<?php
+header('Content-Type: application/json; charset=UTF-8');
+header('X-Content-Type-Options: nosniff');
+if (!isset($_SERVER['REQUEST_METHOD']) || strtoupper($_SERVER['REQUEST_METHOD']) !== 'POST') {
+  http_response_code(405);
+  echo(json_encode(array('state' => 'error', 'message' => 'Ta operacja wymaga metody POST.')));
+  include_once($actual_path."wykonanie_procedur_koncowych.php");
+  exit();
+}
+$action = isset($_POST['action']) ? $_POST['action'] : '';
+$idUser = isset($_POST['id_user']) ? intval($_POST['id_user']) : 0;
+$token = isset($_POST['token']) ? $_POST['token'] : '';
 
-  <?php
-    //header('Content-Type: application/json');
-
-    switch ($_REQUEST['action'])
+switch ($action)
+{
+  case 'add':
+    try{
+      AddToFriendList($idUser, $token);
+      echo(json_encode(array('state' => 'added', 'id_user_friend' => $idUser)));
+    }catch (Exception $e)
     {
-    
-      case 'add':
-      {
-        try{
-          AddToFriendList($_REQUEST['id_user'], $_REQUEST['token']);
-          echo(json_encode(array(
-            'state' => 'added',
-            'id_user_friend' => intval($_REQUEST['id_user'])
-          )));
-        }catch (Exception $e)
-        {
-          echo(json_encode(array(
-            'state' => 'error',
-            'message' => $e->getMessage()
-          )));
-        }
-      
-        break;
-      }
-      
-    
-      case 'remove':
-      {
-        try{
-          DeleteFromFriendList($_REQUEST['id_user'], $_REQUEST['token']);
-          echo(json_encode(array(
-            'state' => 'removed',
-            'id_user_friend' => intval($_REQUEST['id_user'])
-          )));
-          
-        }catch (Exception $e)
-        {
-          echo(json_encode(array(
-            'state' => 'error',
-            'message' => $e->getMessage()
-          )));
-        }      
-        break;
-      }
-          
+      echo(json_encode(array('state' => 'error', 'message' => $e->getMessage())));
     }
-  ?>
+  break;
 
+  case 'remove':
+    try{
+      DeleteFromFriendList($idUser, $token);
+      echo(json_encode(array('state' => 'removed', 'id_user_friend' => $idUser)));
+    }catch (Exception $e)
+    {
+      echo(json_encode(array('state' => 'error', 'message' => $e->getMessage())));
+    }
+  break;
+
+  default:
+    http_response_code(400);
+    echo(json_encode(array('state' => 'error', 'message' => 'Action not supported.')));
+  break;
+}
+?>
 <?php include_once($actual_path."wykonanie_procedur_koncowych.php"); ?>
