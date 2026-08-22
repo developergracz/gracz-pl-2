1c73e5f0795f6cdaba509043bb7b319518de5fb5
developergracz <64929957+developergracz@users.noreply.github.com>
2026-08-22 23:38:55 +0200

    security: escape conversation page output and avoid REQUEST

--- website/conversation.php
+++ website/conversation.php
@@ -14,30 +14,33 @@
     <?php
       try
       {
-        if (isset($_REQUEST['conversation_login'])&&($_REQUEST['conversation_login']!=$_SESSION['login']))
+        $conversationLogin = isset($_GET['conversation_login']) ? $_GET['conversation_login'] : '';
+        if ($conversationLogin !== '' && $conversationLogin !== (isset($_SESSION['login']) ? $_SESSION['login'] : ''))
         {
-
-          $userData = getUserDataFromLogin($_REQUEST['conversation_login']);
+          $userData = getUserDataFromLogin($conversationLogin);
+          $safeLogin = htmlspecialchars($userData['login'], ENT_QUOTES, 'UTF-8');
+          $safeProfileUrl = htmlspecialchars($path['profile'].'-'.rawurlencode($userData['login']), ENT_QUOTES, 'UTF-8');
+          $safeUserId = intval($userData['id']);
 
           echo('
-          <h1>Obecnie prowadzisz rozmowę z graczem <a href="'.$path['profile'].'-'.$userData['login'].'">'.$userData['login'].'</a> </h1>
+          <h1>Obecnie prowadzisz rozmowę z graczem <a href="'.$safeProfileUrl.'">'.$safeLogin.'</a> </h1>
 
           <form action="./" id="conversation_form">
             <div id="conversation_talk">
-              <div class="loader"><img src="'.$directory['design'].'loader.gif" /><br />Ładowanie konwersacji.<br /><progress id="progress_downloaded_messages" value="1" max="100">Proszę czekać...</progress></div>
+              <div class="loader"><img src="'.htmlspecialchars($directory['design'].'loader.gif', ENT_QUOTES, 'UTF-8').'" alt="" /><br />Ładowanie konwersacji.<br /><progress id="progress_downloaded_messages" value="1" max="100">Proszę czekać...</progress></div>
             </div><br />
 
-            <div id="conversation_status_new_messages"><img src="'.$directory['design'].'icon_message.png" style="vertical-align:middle; padding-right:5px; " /> Są nowe, nieprzeczytane wiadomości...</div>
+            <div id="conversation_status_new_messages"><img src="'.htmlspecialchars($directory['design'].'icon_message.png', ENT_QUOTES, 'UTF-8').'" style="vertical-align:middle; padding-right:5px;" alt="" /> Są nowe, nieprzeczytane wiadomości...</div>
 
-            <input type="text" name="conversation_message" id="conversation_message" maxlength="1024" size="105" data-id_interlocutor="'.$userData['id'].'" />
+            <input type="text" name="conversation_message" id="conversation_message" maxlength="1024" size="105" data-id_interlocutor="'.$safeUserId.'" />
             <button type="submit">Wyślij</button><br />
 
             <span id="showDesktopNotifications"><input type="checkbox" name="showDesktopNotificationsCheckbox" id="showDesktopNotificationsCheckbox" '.($_SESSION['show_desktop_notifications']==1?'checked':'').' /><label for="showDesktopNotificationsCheckbox">Pokazuj powiadomienia na pulpicie</label></span><br />
 
             <span id="muteSoundNotifications"><input type="checkbox" name="muteSoundNotificationsCheckbox" id="muteSoundNotificationsCheckbox" '.($_SESSION['play_new_message_sound']==0?'checked':'').' /><label for="muteSoundNotificationsCheckbox">Wycisz dźwięki powiadomienia</label></span><br />
 
-            <div id="conversation_status_sending"><img src="'.$directory['design'].'loader_2.gif" /> Wysyłanie wiadomości...</div>
-            <div id="conversation_status_error"><img src="'.$directory['design'].'icon_error.png" /> <span></span></div>
+            <div id="conversation_status_sending"><img src="'.htmlspecialchars($directory['design'].'loader_2.gif', ENT_QUOTES, 'UTF-8').'" alt="" /> Wysyłanie wiadomości...</div>
+            <div id="conversation_status_error"><img src="'.htmlspecialchars($directory['design'].'icon_error.png', ENT_QUOTES, 'UTF-8').'" alt="" /> <span></span></div>
           </form>
           ');
         }else{
@@ -58,16 +61,15 @@
         </details>
         <br /><br />
 
-        <a href="'.$service_base_address.'" class="button_normal">Strona główna</a>
-        <a href="'.$path['profile'].'" class="button_normal">Twój profil</a>
+        <a href="'.htmlspecialchars($service_base_address, ENT_QUOTES, 'UTF-8').'" class="button_normal">Strona główna</a>
+        <a href="'.htmlspecialchars($path['profile'], ENT_QUOTES, 'UTF-8').'" class="button_normal">Twój profil</a>
         ');
       }catch(Exception $e)
       {
-        echo($e);
+        echo(htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8'));
       }
 
       ?>
-
 
       <br style="clear:both;" />
 
