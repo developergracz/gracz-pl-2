d4d0f294596d0cd705b3a5080714720570972f07
developergracz <64929957+developergracz@users.noreply.github.com>
2026-08-24 10:49:19 +0200

    security: fix password change syntax and harden flow

--- website/zmiana_hasla.php
+++ website/zmiana_hasla.php
@@ -2,65 +2,41 @@
 
 <?php
 
-  echo('
-  <div class="panel_lewy">
-  ');
+  echo('\n  <div class="panel_lewy">\n  ');
 
     WyswietlLewyPanel($_SESSION['account_type']);
 
-  echo('
-  </div>
-  ');
-
+  echo('\n  </div>\n  ');
 
 if ($_SESSION['account_type'] != USER)
 {
-
-  echo('
-  <div class="tresc">
-    <div class="uwaga">Nie zalogowałeś się do tej pory. Jedynie zalogowani użytkownicy mają dostęp do tej strony.</div>
-  </div>
-  ');
-
+  echo('\n  <div class="tresc">\n    <div class="uwaga">Nie zalogowałeś się do tej pory. Jedynie zalogowani użytkownicy mają dostęp do tej strony.</div>\n  </div>\n  ');
 }else
 {
-  echo('
-  <div class="tresc">
+  echo('\n  <div class="tresc">\n\n  <h1>Zmiana Twojego hasła</h1>\n  <h4>Poniższy formularz pozwoli Ci zmienić hasło Twojego konta.</h4>\n  ');
 
-  <h1>Zmiana Twojego hasła</h1>
-  <h4>Poniższy formularz pozwoli Ci zmienić hasło Twojego konta.</h4>
-  ');
-
-  if (isset($_POST['stare_password']))
+  if (isset($_POST['nowe_password_OK']))
   {
     try
     {
-      AccountChangePassword($_SESSION['id'],$_POST['old_password'],$_POST['new_password'], $_POST['new_password_confirm']))
-      echo('<div class="positive">Gratulacje, zmiana hasła powiodła się.</div>');
+      RequireCsrf();
+      SecurityRequireRateLimit('password_change', 5, 900, 1800, isset($_SESSION['id']) ? (string)$_SESSION['id'] : SecurityClientIp());
+      $passwordValidation = SecurityValidatePassword(isset($_POST['new_password']) ? $_POST['new_password'] : '');
+      if ($passwordValidation !== true) {
+        throw new RuntimeException($passwordValidation);
+      }
+      AccountChangePassword($_SESSION['id'], $_POST['old_password'], $_POST['new_password'], $_POST['new_password_confirm']);
+      AuditLog('auth.password_changed', 'user', $_SESSION['id']);
+      echo('<div class="positive">Gratulacje, zmiana hasła powiodła się. Zostaniesz wylogowany, aby unieważnić dotychczasową sesję.</div>');
+      Logout();
       RedirectJavaScript($service_base_address, 1);
     }catch(Exception $e)
     {
-      echo($e);
+      echo('<div class="negative">'.htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8').'</div>');
     }
   }
 
-  echo('
-  <form action="" method="post" class="formularz_wyrownany">
-    <fieldset>
-      <label for="old_password">Stare hasło: </label>
-      <input type="password" name="old_password" id="old_password" /><br />
-      <label for="new_password">Nowe hasło: <br /><small>Powinno mieć przynajmniej '.$minimal_password_length.' znaków.</small></label>
-      <input type="password" name="new_password" id="new_password" /><br />
-      <label for="new_password_confirm">Powtórz nowe hasło: </label>
-      <input type="password" name="new_password_confirm" id="new_password_confirm" /><br />
-
-      <input type="submit" name="nowe_password_OK" value="Zmień hasło" />
-    </fieldset>
-  </form>
-
-  </div>
-  ');
-
+  echo('\n  <form action="" method="post" class="formularz_wyrownany">\n    <fieldset>\n      '.CsrfField().'\n      <label for="old_password">Stare hasło: </label>\n      <input type="password" name="old_password" id="old_password" autocomplete="current-password" /><br />\n      <label for="new_password">Nowe hasło: <br /><small>Co najmniej 12 znaków, mała i wielka litera oraz cyfra.</small></label>\n      <input type="password" name="new_password" id="new_password" autocomplete="new-password" /><br />\n      <label for="new_password_confirm">Powtórz nowe hasło: </label>\n      <input type="password" name="new_password_confirm" id="new_password_confirm" autocomplete="new-password" /><br />\n\n      <input type="submit" name="nowe_password_OK" value="Zmień hasło" />\n    </fieldset>\n  </form>\n\n  </div>\n  ');
 }
 
 ?>
