cd138715aec520c3936508d3218154eb20a157e9
developergracz <64929957+developergracz@users.noreply.github.com>
2026-08-24 10:16:56 +0200

    security: wire newsletter and secure mail services

--- website/security/SecurityBootstrap.php
+++ website/security/SecurityBootstrap.php
@@ -9,6 +9,8 @@ require_once __DIR__.'/UploadSecurityService.php';
 require_once __DIR__.'/ModerationService.php';
 require_once __DIR__.'/TwoFactorService.php';
 require_once __DIR__.'/RbacService.php';
+require_once __DIR__.'/SecureMailService.php';
+require_once __DIR__.'/NewsletterService.php';
 
 SecurityService::sendSecurityHeaders();
 SecurityService::configureSessionCookie();
@@ -18,24 +20,26 @@ function GraczRateLimiter()
     global $database_handle, $database_prefix;
     return new RateLimitService($database_handle, $database_prefix);
 }
-
 function GraczAudit()
 {
     global $database_handle, $database_prefix;
     return new AuditService($database_handle, $database_prefix);
 }
-
+function GraczNewsletter()
+{
+    global $database_handle, $database_prefix;
+    if (!($database_handle instanceof PDO)) throw new RuntimeException('Database connection unavailable.');
+    return new NewsletterService($database_handle, $database_prefix);
+}
 function GraczRequireCsrf()
 {
     return SecurityService::verifyStateChangingRequest();
 }
-
 function GraczRequirePermission($permission)
 {
     global $database_handle, $database_prefix;
     return RbacService::requirePermission($permission, $database_handle, $database_prefix);
 }
-
 function GraczRequireAdmin2fa()
 {
     global $database_handle, $database_prefix;
