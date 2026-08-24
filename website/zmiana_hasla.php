70eee001bd4d83591466f3e860432ff55965fb91
developergracz <64929957+developergracz@users.noreply.github.com>
2026-08-24 10:20:26 +0200

    security: protect password changes and revoke session immediately

--- website/zmiana_hasla.php
+++ website/zmiana_hasla.php
@@ -1,68 +1,42 @@
 <?php include("variables_local.php"); include_once($header); ?>
-
 <?php
+echo('<div class="panel_lewy">');
+WyswietlLewyPanel($_SESSION['account_type']);
+echo('</div>');
 
-  echo('
-  <div class="panel_lewy">
-  ');
-
-    WyswietlLewyPanel($_SESSION['account_type']);
-
-  echo('
-  </div>
-  ');
-
-
-if ($_SESSION['account_type'] != USER)
-{
-
-  echo('
-  <div class="tresc">
-    <div class="uwaga">Nie zalogowałeś się do tej pory. Jedynie zalogowani użytkownicy mają dostęp do tej strony.</div>
-  </div>
-  ');
-
-}else
-{
-  echo('
-  <div class="tresc">
-
-  <h1>Zmiana Twojego hasła</h1>
-  <h4>Poniższy formularz pozwoli Ci zmienić hasło Twojego konta.</h4>
-  ');
-
-  if (isset($_POST['stare_password']))
-  {
-    try
-    {
-      AccountChangePassword($_SESSION['id'],$_POST['old_password'],$_POST['new_password'], $_POST['new_password_confirm']))
-      echo('<div class="positive">Gratulacje, zmiana hasła powiodła się.</div>');
-      RedirectJavaScript($service_base_address, 1);
-    }catch(Exception $e)
-    {
-      echo($e);
+if (empty($_SESSION['initiated']) || $_SESSION['account_type'] < USER) {
+  echo('<div class="tresc"><div class="uwaga">Musisz być zalogowany, aby zmienić hasło.</div></div>');
+} else {
+  echo('<div class="tresc"><h1>Zmiana hasła</h1>');
+  if (isset($_POST['nowe_password_OK'])) {
+    try {
+      SecurityService::verifyStateChangingRequest();
+      GraczRateLimiter()->enforce('password-change-user', (string)$_SESSION['id'], 5, 3600);
+      $new = SecurityService::validatePassword(isset($_POST['new_password']) ? $_POST['new_password'] : '');
+      $confirm = isset($_POST['new_password_confirm']) ? (string)$_POST['new_password_confirm'] : '';
+      if (!hash_equals($new, $confirm)) throw new InvalidArgumentException('Nowe hasła nie są identyczne.');
+      $old = isset($_POST['old_password']) ? (string)$_POST['old_password'] : '';
+      AccountChangePassword($_SESSION['id'], $old, $new, $confirm);
+      $uid = (int)$_SESSION['id'];
+      GraczAudit()->record('auth.password.changed', $uid);
+      // Password change revokes the current browser session immediately. Other persisted sessions
+      // must also be marked revoked in prefix_security_sessions when that migration is enabled.
+      Logout();
+      SecurityService::destroySession();
+      echo('<div class="positive">Hasło zostało zmienione. Ze względów bezpieczeństwa zostałeś wylogowany. Zaloguj się ponownie.</div>');
+    } catch(Exception $e) {
+      GraczAudit()->record('auth.password.change_failed', isset($_SESSION['id'])?$_SESSION['id']:null, array('type'=>get_class($e)), 'warning');
+      echo('<div class="negative">'.htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8').'</div>');
     }
+  } else {
+    echo('<form action="" method="post" class="formularz_wyrownany" autocomplete="off"><fieldset>');
+    echo SecurityService::csrfInput();
+    echo('<label for="old_password">Stare hasło:</label><input type="password" name="old_password" id="old_password" autocomplete="current-password" required><br>');
+    echo('<label for="new_password">Nowe hasło:<br><small>Minimum 12 znaków.</small></label><input type="password" name="new_password" id="new_password" minlength="12" maxlength="128" autocomplete="new-password" required><br>');
+    echo('<label for="new_password_confirm">Powtórz nowe hasło:</label><input type="password" name="new_password_confirm" id="new_password_confirm" minlength="12" maxlength="128" autocomplete="new-password" required><br>');
+    echo('<button type="submit" name="nowe_password_OK" value="1">Zmień hasło</button></fieldset></form>');
   }
-
-  echo('
-  <form action="" method="post" class="formularz_wyrownany">
-    <fieldset>
-      <label for="old_password">Stare hasło: </label>
-      <input type="password" name="old_password" id="old_password" /><br />
-      <label for="new_password">Nowe hasło: <br /><small>Powinno mieć przynajmniej '.$minimal_password_length.' znaków.</small></label>
-      <input type="password" name="new_password" id="new_password" /><br />
-      <label for="new_password_confirm">Powtórz nowe hasło: </label>
-      <input type="password" name="new_password_confirm" id="new_password_confirm" /><br />
-
-      <input type="submit" name="nowe_password_OK" value="Zmień hasło" />
-    </fieldset>
-  </form>
-
-  </div>
-  ');
-
+  echo('</div>');
 }
-
 ?>
-
 <?php include_once($footer); ?>
